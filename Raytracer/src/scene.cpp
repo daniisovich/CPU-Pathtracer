990abf1f524736c87eecb8040c93bc90b4ad135e
unknown <daniel.preuss93@gmail.com>
2022-06-07 12:42:56 +0200

    Implemented constructor and addObjects method of Scene class

--- Raytracer/src/scene.cpp
+++ Raytracer/src/scene.cpp
@@ -1,16 +1,27 @@
 #include "scene.h"
 
 
+Scene::Scene(const std::vector<std::shared_ptr<SceneObject>>& objects) : m_objects{ objects } { }
+
 void Scene::addObject(const std::shared_ptr<SceneObject> object) {
-	objects.push_back(object);
+	m_objects.push_back(object);
+}
+
+void Scene::addObjects(const std::vector<std::shared_ptr<SceneObject>>& objects) {
+	
+	m_objects.reserve(m_objects.size() + objects.size());
+	for (const auto& object : objects) {
+		m_objects.push_back(object);
+	}
+
 }
 
 std::optional<Hit> Scene::intersect(const Ray& ray, float near, float far) const {
 
 	std::optional<Hit> intersection;
-	for (size_t i{ 0 }; i < objects.size(); ++i) {
+	for (size_t i{ 0 }; i < m_objects.size(); ++i) {
 
-		auto result{ objects[i]->intersect(ray, near, far) };
+		const auto result{ m_objects[i]->intersect(ray, near, far) };
 		if (!result.has_value() || (intersection.has_value() && result.value().t >= intersection.value().t)) continue;
 
 		intersection = result;
--- Raytracer/src/scene.h
+++ Raytracer/src/scene.h
@@ -11,13 +11,15 @@ class Scene {
 public:
 
 	Scene() = default;
-	
+	Scene(const std::vector<std::shared_ptr<SceneObject>>& objects);
+
 	void addObject(const std::shared_ptr<SceneObject> object);
-	void addObjects(const std::vector<std::shared_ptr<SceneObject>> objects);
+	void addObjects(const std::vector<std::shared_ptr<SceneObject>>& objects);
+
 	std::optional<Hit> intersect(const Ray& ray, float near, float far) const;
 
 private:
 
-	std::vector<std::shared_ptr<SceneObject>> objects{};
+	std::vector<std::shared_ptr<SceneObject>> m_objects{};
 
 };
