ddbe9ecc45e4dd7a005de335f00aa1201fcffde6
unknown <daniel.preuss93@gmail.com>
2022-06-10 21:00:48 +0200

    Changed intersection calculation to not use normalized ray direction, otherwise one would need to use normalized ray direction in other parts

--- Raytracer/src/sphere.cpp
+++ Raytracer/src/sphere.cpp
@@ -1,18 +1,19 @@
 #include "sphere.h"
 
 
-const std::optional<std::pair<float, float>> solveQuadratic(const float& half_b, const float& c);
+const std::optional<std::pair<float, float>> solveQuadratic(float a, float half_b, float c);
 
 
 Sphere::Sphere(const Vec3& center, float radius, const std::shared_ptr<Material>& material) : m_center{ center }, m_radius{ radius }, m_material{ material } { }
 
 const std::optional<Hit> Sphere::intersect(const Ray& ray, float near, float far) const {
 
-	const Vec3 oc{ ray.origin() - m_center };
-	const float half_b{ Vec3::dot(oc, Vec3::normalize(ray.direction())) };
-	const float c{ oc.sq_length() - m_radius * m_radius };
+	const Vec3 co{ ray.origin() - m_center };
+	const float a{ ray.direction().sq_length() };
+	const float half_b{ Vec3::dot(co, ray.direction()) };
+	const float c{ co.sq_length() - m_radius * m_radius };
 
-	const auto result = solveQuadratic(half_b, c);
+	const auto result = solveQuadratic(a, half_b, c);
 	// ray doesn't hit object
 	if (!result.has_value()) return {};
 	const auto[t1, t2] = result.value();
@@ -33,22 +34,23 @@ const std::optional<Hit> Sphere::intersect(const Ray& ray, float near, float far
 const Hit Sphere::getIntersection(const Ray& ray, float t) const {
 
 	const Vec3 intersection{ ray.point(t) };
-	Vec3 out_normal{ (m_center.invert() + intersection) / m_radius };
+	const Vec3 out_normal{ (intersection - m_center) / m_radius };
 	Hit hit{ t, intersection, m_material };
 	hit.set_normal(ray.direction(), out_normal);
 	return hit;
 
 }
 
-const std::optional<std::pair<float, float>> solveQuadratic(const float& half_b, const float& c) {
+const std::optional<std::pair<float, float>> solveQuadratic(float a, float half_b, float c) {
 
-	const float discriminant = half_b * half_b - c;
+	const float discriminant = half_b * half_b - a * c;
 	if (discriminant < 0) return {};
-	if (discriminant == 0) return std::pair{ half_b, half_b };
+	if (discriminant == 0) return std::pair{ half_b / a, half_b / a };
 
 	const float sqrt_disc{ sqrt(discriminant) };
-	float t1 = (half_b > 0) ? -half_b - sqrt_disc : -half_b + sqrt_disc;
-	float t2{ c / t1 };
+	const float q = (half_b > 0) ? - half_b - sqrt_disc : - half_b + sqrt_disc;
+	float t1{ q / a };
+	float t2{ c / q };
 
 	if (t1 > t2) std::swap(t1, t2);
 	return std::pair{ t1, t2 };
