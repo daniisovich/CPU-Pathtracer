31302d0245df6f54587c4694a047c2f1aac80ef9
unknown <daniel.preuss93@gmail.com>
2022-06-13 15:14:51 +0200

    Changed BRDFFunction to BRDF; Added reflectiveness getter

--- Raytracer/src/scene/material.cpp
+++ Raytracer/src/scene/material.cpp
@@ -3,7 +3,7 @@
 #include "../utility/utility.h"
 
 
-Material::Material(const Vec3& color, float reflectiveness, float fuzziness, BRDFFunction brdf) : m_color{ color }, m_reflectiveness{ reflectiveness }, m_fuzziness{ fuzziness }, m_brdf{ brdf } { }
+Material::Material(const Vec3& color, float reflectiveness, float fuzziness, BRDF brdf) : m_color{ color }, m_reflectiveness{ reflectiveness }, m_fuzziness{ fuzziness }, m_brdf{ brdf } { }
 
 std::pair<Vec3, std::optional<Vec3>> Material::scatter(const Vec3& ray_direction, const Vec3& normal) const {
 
@@ -14,3 +14,4 @@ std::pair<Vec3, std::optional<Vec3>> Material::scatter(const Vec3& ray_direction
 }
 
 const Vec3 Material::color() const { return m_color; }
+float Material::reflectiveness() const { return m_reflectiveness; }
--- Raytracer/src/scene/material.h
+++ Raytracer/src/scene/material.h
@@ -6,22 +6,23 @@
 #include "../core/ray.h"
 
 
-using BRDFFunction = std::function<Vec3 (const Vec3&, const Vec3&)>;
+using BRDF = std::function<Vec3 (const Vec3&, const Vec3&)>;
 
 class Material {
 
 public:
 
-	Material(const Vec3& color, float reflectiveness, float fuzziness, BRDFFunction brdf);
+	Material(const Vec3& color, float reflectiveness, float fuzziness, BRDF brdf);
 
 	virtual std::pair<Vec3, std::optional<Vec3>> scatter(const Vec3& ray_direction, const Vec3& normal) const;
 
 	const Vec3 color() const;
+	float reflectiveness() const;
 
 private:
 
 	Vec3 m_color;
 	float m_reflectiveness, m_fuzziness;
-	BRDFFunction m_brdf;
+	BRDF m_brdf;
 
 };
