397fa2fe42f2b78484644fa37842fb4c32434fa8
unknown <daniel.preuss93@gmail.com>
2022-06-19 12:04:12 +0200

    Changed shadow ray intersection to stop epsilon units before lightsource to prevent selfintersections

--- Raytracer/src/core/intersection.cpp
+++ Raytracer/src/core/intersection.cpp
@@ -15,22 +15,22 @@ Vec3 Intersection::lighting(const Scene& scene, float epsilon) const {
 	for (const auto& light : scene.lights()) {
 		Vec3 lightColor{ light->color() };
 		Ray lightray{ light->lightRay(m_position) };
-		if (scene.intersect(lightray, epsilon, 1.0f).has_value()) continue;
+		if (scene.intersect(lightray, epsilon, 1.0f - epsilon).has_value()) continue;
 
 		const Vec3 lightDir{ Vec3::normalize(lightray.direction()) };
 		float weight{ Vec3::dot(lightDir, m_normal) };
 		if (weight < 0.0f) continue;
-		diffuse += 0.5f * weight * m_material->color() * lightColor;
+		diffuse += 0.3f * weight * m_material->color() * lightColor;
 
 		const Vec3 reflectedLight{ Vec3::reflect(-lightDir, m_normal) };
 
 		weight = Vec3::dot(reflectedLight, m_view_dir);
 		if (weight < 0.0f) continue;
-		specular += 0.3f * std::powf(weight, 10) * m_material->color() * lightColor;
+		specular += 0.2f * std::powf(weight, 10) * m_material->color() * lightColor;
 
 	}
 
-	Vec3 ambient{ 0.2f * m_material->color() };
+	Vec3 ambient{ 0.5f * m_material->color() };
 	return ambient + (diffuse + specular) / float(scene.lights().size());
 
 }
